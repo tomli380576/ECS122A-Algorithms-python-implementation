4bf1d60c87a379c814bc8c709dc1f660227c4c7c
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-06-24 16:23:53 -0700

    fixed incorrect max call

--- class-scheduling-backtracking.py
+++ class-scheduling-backtracking.py
@@ -1,4 +1,5 @@
 import math
+from typing import Union
 
 START_TIMES = {'A': 1, 'B': 2, 'C': 6, 'D': 3}
 FINISH_TIMES = {'A': 5, 'B': 4, 'C': 8, 'D': 9}
@@ -14,9 +15,9 @@ def sortDictByValue(x: dict[str, int]):
 
 
 def RecursiveScheduling_returnCount(start_times: dict[str, int],
-                        finish_times: dict[str, int],
-                        prev_class_finish_time: int,
-                        remaining_classes: set[str]) -> int:
+                                    finish_times: dict[str, int],
+                                    prev_class_finish_time: Union[int, float],
+                                    remaining_classes: set[str]) -> int:
     if len(remaining_classes) == 0:
         return 0
 
@@ -34,14 +35,15 @@ def RecursiveScheduling_returnCount(start_times: dict[str, int],
                 prev_class_finish_time=prev_class_finish_time,
                 remaining_classes=remaining_classes - set(curr_class))
 
-            best = max(take, skip)
+            best = max(take, skip, best)
 
     return best
 
+
 def RecursiveScheduling_returnList(start_times: dict[str, int],
-                        finish_times: dict[str, int],
-                        prev_class_finish_time: int,
-                        remaining_classes: set[str]) -> list:
+                                   finish_times: dict[str, int],
+                                   prev_class_finish_time: Union[int, float],
+                                   remaining_classes: set[str]) -> list:
     if len(remaining_classes) == 0:
         return []
 
@@ -54,22 +56,29 @@ def RecursiveScheduling_returnList(start_times: dict[str, int],
                 start_times, finish_times,
                 prev_class_finish_time=finish_times[curr_class],
                 remaining_classes=remaining_classes - set(curr_class)) + [curr_class]
+
             skip = RecursiveScheduling_returnList(
                 start_times, finish_times,
                 prev_class_finish_time=prev_class_finish_time,
                 remaining_classes=remaining_classes - set(curr_class))
 
+            if max(len(take), len(skip)) <= len(best_schedule):
+                continue
             if len(take) >= len(skip):
                 best_schedule = take
             else:
                 best_schedule = skip
-    
-    best_schedule.reverse()
+
     return best_schedule
 
 
 if __name__ == '__main__':
     ALL_CLASSES = set(FINISH_TIMES.keys())
-    # print('best')
-    print(RecursiveScheduling_returnList(START_TIMES, FINISH_TIMES, -math.inf, ALL_CLASSES))
-    # print([3][1:])
+    # Since we are using a set, the order might be messed up
+    # Sort the schedule by starting time if necessary
+    best_schedule = RecursiveScheduling_returnList(
+        START_TIMES, FINISH_TIMES, -math.inf, ALL_CLASSES) 
+    best_schedule.sort(key=START_TIMES.get)  # type: ignore
+    best_count = RecursiveScheduling_returnCount(
+        START_TIMES, FINISH_TIMES, -math.inf, ALL_CLASSES) 
+    print(f'The best schedule is: {best_schedule}, length: {best_count}')
