09e3e3695acf4aa7df39dfd7118d243e79f9ce95
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-06-24 19:29:31 -0700

    add generic search

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# 122Algorithms-python-implementation
+# 122A Algorithms: `python` implementation
+
 My implementations of all the fundamental algorithms covered in ECS122A
---
+++ whateverFirstSearch.py
@@ -0,0 +1,61 @@
+from queue import PriorityQueue, Queue, LifoQueue
+from typing import Union
+from enum import Enum
+
+
+class STATUS(Enum):
+    DISCOVERED = 1
+    NOT_DISCOVERED = 2
+    FINISHED = 3
+
+
+Bag = Union[LifoQueue, Queue, PriorityQueue]
+
+VISIT_STATUS: dict[str, STATUS] = {}
+SAMPLE_DIRECTED_GRAPH: dict[str, list[str]] = {
+    '5': ['3', '7'],
+    '3': ['2', '4'],
+    '7': ['8'],
+    '2': [],
+    '4': ['8'],
+    '8': []
+}
+SAMPLE_UNDIRECTED_GRAPH: dict[str, list[str]] = {
+    '5': ['3', '7'],
+    '3': ['2', '4', '5'],
+    '7': ['8', '5'],
+    '2': ['3'],
+    '4': ['8', '3'],
+    '8': ['4', '7']
+}
+SAMPLE_UNDIRECTED_GRAPH2: dict[str, list[str]] = {
+    'A': ['B', 'C', 'E'],
+    'C': ['A', 'B', 'D', 'E'],
+    'B': ['A', 'C', 'D'],
+    'E': ['A', 'C'],
+    'D': ['B', 'C']
+}
+
+
+def whateverFirstSearch(G, start_vertex: str, data_structure: Bag):
+    for vertex in G:
+        VISIT_STATUS[vertex] = STATUS.NOT_DISCOVERED
+
+    data_structure.put(start_vertex)
+    while not data_structure.empty():
+        vertex = data_structure.get()
+        if VISIT_STATUS[vertex] == STATUS.NOT_DISCOVERED:
+            print(f'Newly Discovered {vertex}')
+            VISIT_STATUS[vertex] = STATUS.DISCOVERED
+            for adjacent_vertex in G[vertex]:
+                data_structure.put(adjacent_vertex)
+
+
+if __name__ == '__main__':
+    stack = LifoQueue()
+    queue = Queue()
+    priority_queue = PriorityQueue()
+
+    whateverFirstSearch(SAMPLE_UNDIRECTED_GRAPH, '3', priority_queue)
+
+    pass
