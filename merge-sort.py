7718b0fd06e5413d90dc631ce01eabe90faf9b97
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-07-15 22:52:23 -0700

    example in merge sort

--- merge-sort.py
+++ merge-sort.py
@@ -9,13 +9,27 @@ def sortDescending(x, y):
     return x > y
 
 
+def alphabeticalOrder(s1: str, s2: str) -> bool:
+    shorter_len = min(len(s1), len(s2))
+    for i in range(shorter_len):  # O(length of shorter string)
+        if ord(s1[i]) == ord(s2[i]):
+            continue
+        elif ord(s1[i]) > ord(s2[i]):
+            return False
+        elif ord(s1[i]) < ord(s2[i]):
+            return True
+    return True
+
+
 def merge2SortedArrays(arr1: list, arr2: list,
                        predicate: Callable[[Any, Any], bool]) -> list:
     ptr1, ptr2 = 0, 0
     out = []
 
-    while ptr1 != len(arr1) and ptr2 != len(arr2):
-        if predicate(arr1[ptr1], arr2[ptr2]):
+    while ptr1 != len(arr1) and ptr2 != len(arr2):  # O(n)
+        if predicate(
+                arr1[ptr1], arr2[ptr2]
+        ):  # O(perdicate)       comparing 2 strings: O(n), comparing 2 numbers: O(1)
             out.append(arr1[ptr1])
             ptr1 += 1
         else:
@@ -45,8 +59,15 @@ def mergeSort(arr: list, predicate: Callable[[Any, Any], bool]) -> list:
 
 
 if __name__ == '__main__':
-    sample_arr = [
+    int_arr1 = [
         3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6
     ]
-    print(f'Original: {sample_arr}')
-    print(f'Sorted:   {mergeSort(sample_arr, sortAscending)}')
+
+    str_arr1 = [
+        'qowieuoqiw', 'askdhgasdkjhf', 'jnkasbruw', 'quwetrioeio',
+        'dhgasdkjhfasdad', 'jsbrupdofsfhns', 'qwyegqdjkh'
+    ]
+    str_arr2 = ['aaaaab', 'aaaaaa']
+
+    print(f'Sorted numbers: {mergeSort(int_arr1, sortAscending)}')
+    print(f'Sorted strings: {mergeSort(str_arr2, alphabeticalOrder)}')
