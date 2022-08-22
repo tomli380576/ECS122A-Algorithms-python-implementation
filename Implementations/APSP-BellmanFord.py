f05c9c89f269548019ccc9af448cbeaa70c4199e
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-08-22 14:12:48 -0700

    apsp bellman ford implementaion

---
+++ Implementations/APSP-BellmanFord.py
@@ -0,0 +1,41 @@
+from math import inf
+from example_weighted_graphs import W_DIRECTED_1
+from graph_helpers import WeightedGraph, GetWeightedEdges
+
+
+def APSPBellmanFord(G: WeightedGraph):
+    dist = {v: {u: 0 for u in G.keys()} for v in G.keys()}
+
+    # Fill in base cases
+    for u in G.keys():
+        for v in G.keys():
+            if u == v:
+                dist[u][v] = 0
+            else:
+                dist[u][v] = inf  # type: ignore
+
+    # Iterate over all u and edges
+    for max_edges in range(len(G.keys()) - 1):
+        for u in G.keys():
+            for edge in GetWeightedEdges(G):
+                weight, x, v = edge
+                # if tense
+                if dist[u][v] > dist[u][x] + weight:
+                    # Relax
+                    dist[u][v] = dist[u][x] + weight
+
+    return dist
+
+
+if __name__ == '__main__':
+    dist = APSPBellmanFord(W_DIRECTED_1)
+
+    # Pretty Printing 2D Dict Driver Code
+    for key in dist.keys():
+        print(f'\t{key}', end='')
+    print('\n')
+    for idx, u in enumerate(dist.keys()):
+        print(f'{u}', end='')
+        for v in dist.keys():
+            print(f'\t{dist[u][v]}', end='')
+        print('\n')
